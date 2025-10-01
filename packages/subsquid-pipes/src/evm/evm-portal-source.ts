cf527b638653df9178ccc80e4a687af6b2b3bb38
Eugene Formanenko <mo4islona@gmail.com>
2025-10-01 12:21:41 +0400

    refactor: enforce logger requirement and streamline transformer handling in PortalSource

--- packages/subsquid-pipes/src/evm/evm-portal-source.ts
+++ packages/subsquid-pipes/src/evm/evm-portal-source.ts
@@ -1,5 +1,13 @@
-import { Logger, PortalRange, PortalSource, parsePortalRange, Transformer } from '~/core/index.js'
-import { ProgressTrackerOptions } from '~/core/progress-tracker.js'
+import {
+  createDefaultLogger,
+  createTransformer,
+  Logger,
+  PortalRange,
+  PortalSource,
+  parsePortalRange,
+  Transformer,
+} from '~/core/index.js'
+import { ProgressTrackerOptions, progressTracker } from '~/core/progress-tracker.js'
 import { PortalCacheOptions } from '~/portal-cache/portal-cache.js'
 import { evm, PortalClient, PortalClientOptions } from '../portal-client/index.js'
 import { EvmQueryBuilder } from './evm-query-builder.js'
@@ -7,6 +15,8 @@ import { EvmQueryBuilder } from './evm-query-builder.js'
 export type EvmTransformer<In, Out> = Transformer<In, Out, EvmQueryBuilder>
 
 export type EvmPortalData<F extends evm.FieldSelection> = { blocks: evm.Block<F>[] }
+
+type DefaultData = { blocks: any[] }
 
 export function createEvmPortalSource({
   portal,
@@ -19,30 +29,40 @@ export function createEvmPortalSource({
   query?: PortalRange | EvmQueryBuilder
   cache?: PortalCacheOptions
   logger?: Logger
-  progress?: ProgressTrackerOptions | false
+  progress?: ProgressTrackerOptions
 }) {
   if (query && !(query instanceof EvmQueryBuilder)) {
     query = new EvmQueryBuilder().addRange(parsePortalRange(query))
   }
 
-  return new PortalSource<EvmQueryBuilder, { blocks: any[] }>({
+  logger = logger || createDefaultLogger()
+
+  return new PortalSource<EvmQueryBuilder, DefaultData>({
     portal,
     query: query || new EvmQueryBuilder(),
     cache,
     logger,
-    progress,
-  }).pipe({
-    profiler: { id: 'normalize_data' },
-    transform: (data) => {
-      data.blocks = data.blocks.map((block) => ({
-        ...block,
-        logs: block.logs || [],
-        transactions: block.transactions || [],
-        stateDiffs: block.stateDiffs || [],
-        traces: block.traces || [],
-      }))
+    transformers: [
+      progressTracker({
+        logger: logger,
+        interval: progress?.interval,
+        onStart: progress?.onStart,
+        onProgress: progress?.onProgress,
+      }),
+      createTransformer<DefaultData, DefaultData>({
+        profiler: { id: 'normalize data' },
+        transform: (data) => {
+          data.blocks = data.blocks.map((block) => ({
+            ...block,
+            logs: block.logs || [],
+            transactions: block.transactions || [],
+            stateDiffs: block.stateDiffs || [],
+            traces: block.traces || [],
+          }))
 
-      return data
-    },
+          return data
+        },
+      }),
+    ],
   })
 }
