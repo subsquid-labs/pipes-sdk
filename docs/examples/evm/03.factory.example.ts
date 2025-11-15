926a34c7ef53a64a4b68822856ea6cc0ab409245
Eugene Formanenko <mo4islona@gmail.com>
2025-11-15 15:01:40 +0400

    feat: replace create-prefixed utility functions with shorter aliases and add deprecation warnings

--- docs/examples/evm/03.factory.example.ts
+++ docs/examples/evm/03.factory.example.ts
@@ -1,10 +1,4 @@
-import {
-  createEvmDecoder,
-  createEvmPortalSource,
-  DecodedEvent,
-  factory,
-  factorySqliteDatabase,
-} from '@sqd-pipes/pipes/evm'
+import { DecodedEvent, evmDecoder, evmPortalSource, factory, factorySqliteDatabase } from '@sqd-pipes/pipes/evm'
 import { events as factoryAbi } from './abi/uniswap.v3/factory'
 import { events as swapsAbi } from './abi/uniswap.v3/swaps'
 
@@ -23,10 +17,10 @@ export function transform<T, F>(event: DecodedEvent<T, F>) {
 }
 
 async function cli() {
-  const stream = createEvmPortalSource({
+  const stream = evmPortalSource({
     portal: 'https://portal.sqd.dev/datasets/ethereum-mainnet',
   }).pipe(
-    createEvmDecoder({
+    evmDecoder({
       range: { from: '12,369,621' },
       contracts: factory({
         address: '0x1f98431c8ad98523631ae4a59f267346ea31f984',
