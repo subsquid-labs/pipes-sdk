15da61f1270012de9cb2c2e5a80ca55aaaf293bc
iankressin <ianguimaraes31@gmail.com>
2026-02-24 18:00:22 -0300

    feat(pipe-ui): use skeleton and smooth transitions to avoid initial page flickering

--- packages/pipe-ui/app/dashboard/dashboard.tsx
+++ packages/pipe-ui/app/dashboard/dashboard.tsx
@@ -5,20 +5,22 @@ import { ArrowUpRightIcon } from 'lucide-react'
 import { useStats } from '~/api/metrics'
 import { Button } from '~/components/ui/button'
 import { Logo } from '~/components/ui/logo'
+import { DashboardSkeleton } from '~/dashboard/dashboard-skeleton'
 import { Pipeline } from '~/dashboard/pipeline'
 import { Sidebar } from '~/dashboard/sidebar'
 
 export function Dashboard() {
-  const { data: stats } = useStats()
-
+  const { data: stats, isLoading } = useStats()
   const [selectedPipe, setSelectedPipe] = useState<string | null>(null)
+
+  if (isLoading) return <DashboardSkeleton />
   const pipes = stats?.pipes || []
 
   // TODO handle case when selected pipe is not in the list anymore (e.g. deleted)
   const pipeId = selectedPipe || pipes?.[0]?.id
 
   return (
-    <div className="flex flex-col items-center pt-16 pb-4 gap-10">
+    <div className="flex flex-col items-center pt-16 pb-4 gap-10 content-fade-in">
       <div className="max-w-[1000px] w-full">
         <div className="flex justify-between">
           <div className="flex self-start mb-8">
